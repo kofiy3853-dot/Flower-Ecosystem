a2ec40d78b947bdf347d3378016f1d7402066b17
Nana <kofikofiy3853@gmail.com>
2026-07-02 03:15:39 +0000

    Add following page, route, and link in account sidebar. Fix emojis on account page.

--- server.js
+++ server.js
@@ -159,6 +159,7 @@ app.get('/search', (_, res) => res.sendFile(path.join(__dirname, 'search.html'))
 app.get('/forgot-password', (_, res) => res.sendFile(path.join(__dirname, 'forgot-password.html')));
 app.get('/contact', (_, res) => res.sendFile(path.join(__dirname, 'contact.html')));
 app.get('/account', (_, res) => res.sendFile(path.join(__dirname, 'account.html')));
+app.get('/following', (_, res) => res.sendFile(path.join(__dirname, 'following.html')));
 app.get('/terms', (_, res) => res.sendFile(path.join(__dirname, 'terms.html')));
 app.get('/privacy', (_, res) => res.sendFile(path.join(__dirname, 'privacy.html')));
 
